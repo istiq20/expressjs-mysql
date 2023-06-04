6a550d50ad710700f2569d7ba74bbcb85c48d96c
iis istiqomah <iisistiqomah20@gmail.com>
2023-06-04 19:45:20 +0700

    refactor middleware log request

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const usersRoutes = require('./routes/users.js');
+const middlewareLogRequest = require('./middleware/logs.js');
 
 const app = express();
 
 const PORT = 8000;
 
 // middleware that is specific to this router
-app.use('/', (req, res, next) => {
-    console.log('Log terjadi request ke API ini');
-    next();
-});
+app.use(middlewareLogRequest);
 
 // app.METHOD(PATH, HANDLER)
 app.use('/users', usersRoutes);
