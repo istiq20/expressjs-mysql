af8dfd8769e8d4f1446e0a56cf075f10987f674a
iis istiqomah <iisistiqomah20@gmail.com>
2023-06-04 18:17:19 +0700

    basic routing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ const app = express();
 
 const PORT = 8000;
 
+// app.METHOD(PATH, HANDLER)
+app.get('/', (req, res) => {
+    res.send('Hello GET method');
+});
+
+app.post('/', (req, res) => {
+    res.send('Hello POST method');
+});
+
 app.listen(PORT, () => {
     console.log(`Server berhasil di running di PORT: ${PORT}`);
 })
