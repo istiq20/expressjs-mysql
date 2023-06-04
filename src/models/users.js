283e0912f24113f5a7ea10d4db0c6e50b4a0b845
iis istiqomah <iisistiqomah20@gmail.com>
2023-06-04 23:38:45 +0700

    update-patch users to MySql database

--- src/models/users.js
+++ src/models/users.js
@@ -13,7 +13,16 @@ const createNewUser = (body) => {
     return dbPool.execute(SQLquery);
 }
 
+const updateUser = (body, idUser) => {
+    const SQLquery = ` UPDATE users
+                       SET name='${body.name}', email='${body.email}', address='${body.address}'
+                       WHERE id=${idUser}`;
+    
+    return dbPool.execute(SQLquery);
+}
+
 module.exports = {
     getAllUsers,
-    createNewUser
+    createNewUser,
+    updateUser
 };
