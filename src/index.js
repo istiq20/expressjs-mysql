b65fa8b619391b8038745e7ddd098c681937fd2f
iis istiqomah <iisistiqomah20@gmail.com>
2023-06-04 21:05:47 +0700

    create connection express to mysql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const usersRoutes = require('./routes/users.js');
 const middlewareLogRequest = require('./middleware/logs.js');
+const mysql = require('mysql2');
 
 const app = express();
 const PORT = 8000;
@@ -10,8 +11,29 @@ app.use(express.json());
 // middleware that is specific to this router
 app.use(middlewareLogRequest);
 
+const dbPool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'expressjs_mysql'
+});
+
 // app.METHOD(PATH, HANDLER)
 app.use('/users', usersRoutes);
+
+app.use('/', (req, res) => {
+    dbPool.execute('SELECT * FROM users', (err, rows) => {
+        if(err) {
+            res.json({
+                message: 'Connection failed'
+            })
+        }
+
+        res.json({
+            message: 'Connection succes',
+            data: rows
+        })
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`Server berhasil di running di PORT: ${PORT}`);
